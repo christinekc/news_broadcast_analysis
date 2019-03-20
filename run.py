cfd791cac5253231d985ccf28773acdf79da24e1
christinekc <christinekc.cheng@gmail.com>
2019-03-20 01:20:13 -0400

    Add combine video

--- run.py
+++ run.py
@@ -8,12 +8,20 @@ def main():
 @main.command()
 @click.option("--data-dir", "-d", type=Path)
 def train(**kwargs):
-	from train import train_model
+    from train import train_model
     train_model(**kwargs)
 
 @main.command()
 def pipeline(**kwargs):
-	pass
+    pass
+
+@main.command()
+@click.option("--imgs_dir", "-d", default="original_data/clip_1/", type=Path)
+@click.option("--vid_name", "-v", default="clip_1")
+@click.option("--fps", "-fps", default=6, type=int, help="Frame per second")
+def make_video(**kwargs):
+    from utils import make_video
+    make_video(**kwargs)
 
 if __name__ == "__main__":
     main()
---
+++ utils.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+
+def make_video(imgs_dir, vid_name, fps):
+    """
+    Make vid_name.mp4 using images in imgs_dir.
+    """
+    OUTPUT_DIR = "output/"
+    if not os.path.isdir(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
+    imgs = [img for img in os.listdir(imgs_dir) if img.endswith(".jpg")]
+    # Sort images by name in ascending order
+    imgs.sort(key=lambda img: int("".join(filter(str.isdigit, img))))
+    frame = cv2.imread(os.path.join(imgs_dir, imgs[0]))
+    h, w, _ = frame.shape
+
+    # Make sure vid_name does not include a file extension
+    index = vid_name.find(".")
+    if index != -1:
+        vid_name = vid_name[:index]
+    file_path = OUTPUT_DIR + vid_name + ".mp4"
+    vid = cv2.VideoWriter(file_path, cv2.VideoWriter_fourcc(*"MP4V"), fps, (w, h))
+    for img in imgs:
+        vid.write(cv2.imread(os.path.join(imgs_dir, img)))
+    vid.release()
+    print("Video is now in ", file_path)
