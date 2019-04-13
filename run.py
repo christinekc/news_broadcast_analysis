bdf93f24dd970d18173a8f6c92477b6a3e73925d
christinekc <christinekc.cheng@gmail.com>
2019-04-13 15:31:29 -0400

    Update run with add shot number

--- run.py
+++ run.py
@@ -6,30 +6,30 @@ def main():
     pass
 
 @main.command()
-@click.option("--model_path", "-m", type=Path, default="cnn_model.h5")
-# @click.option("--model_path", "-m", type=Path, default="svm_model.sav")
+@click.option("--model_path", "-m", type=Path, default="models/cnn_model.h5")
+# @click.option("--model_path", "-m", type=Path, default="models/svm_model.sav")
 @click.option("--classification", "-c", default="CNN", help="SVM or NN_SIFT or CNN")
 def train(**kwargs):
     from face import train_model
     train_model(**kwargs)
 
 @main.command()
-@click.option("--model_path", "-m", type=Path, default="nn_model.h5")
-# @click.option("--model_path", "-m", type=Path, default="svm_model.sav")
+@click.option("--model_path", "-m", type=Path, default="models/nn_model.h5")
+# @click.option("--model_path", "-m", type=Path, default="models/svm_model.sav")
 def predict(**kwargs):
     from face import predict_model
     predict_model(**kwargs)
 
 @main.command()
-@click.option("--model_path", "-m", type=Path, default="svm_model.sav")
-# @click.option("--model_path", "-m", type=Path, default="nn_model.h5")
+@click.option("--model_path", "-m", type=Path, default="models/svm_model.sav")
+# @click.option("--model_path", "-m", type=Path, default="models/nn_model.h5")
 @click.option("--classification", "-c", default="SVM", help="SVM or NN_SIFT or CNN")
 # Clip 1
-# @click.option("--input_dir", "-i", type=Path, default="original_data/clip_1/")
-# @click.option("--output_dir", "-o", type=Path, default="output/clip_1_face/")
-# Clip 2
 @click.option("--input_dir", "-i", type=Path, default="original_data/clip_1/")
-@click.option("--output_dir", "-o", type=Path, default="output/clip_1/")
+@click.option("--output_dir", "-o", type=Path, default="output/clip_1_face/")
+# Clip 2
+# @click.option("--input_dir", "-i", type=Path, default="original_data/clip_2/")
+# @click.option("--output_dir", "-o", type=Path, default="output/clip_2/")
 def face_detection(**kwargs):
     from face import face_detection_hsv, face_detection_cascade
     # face_detection_hsv(**kwargs)
@@ -56,8 +56,9 @@ def logo_detection(**kwargs):
     logo_detection(**kwargs)
 
 @main.command()
-@click.option("--input_dir", "-i", type=Path, default="original_data/clip_1/")
-@click.option("--type", "-t", default="HD", help="SAD2 or HD")
+@click.option("--input_dir", "-i", type=Path, default="original_data/clip_3/")
+@click.option("--method", "-m", default="SAD2", help="SAD2 or HD")
+@click.option("--k", "-k", default=1, help="k used in threshold")
 def shot_detection(**kwargs):
     from shot import shot_detection
     shot_detection(**kwargs)
@@ -73,28 +74,45 @@ def make_video(**kwargs):
 @main.command()
 # Clip 1
 # @click.option("--input_dir", "-i", type=Path, default="original_data/clip_1/")
+# @click.option("--output_dir", "-o", type=Path, default="output/clip_1_shots/")
+# @click.option("--shots", "-s", type=list, default=[22, 156])
+# Clip 2
+# @click.option("--input_dir", "-i", type=Path, default="original_data/clip_2/")
+# @click.option("--output_dir", "-o", type=Path, default="output/clip_2_shots/")
+# @click.option("--shots", "-s", type=list, default=[65, 120, 138, 144, 152, 164, 177, 188])
+# Clip 3
+@click.option("--input_dir", "-i", type=Path, default="original_data/clip_3/")
+@click.option("--output_dir", "-o", type=Path, default="output/clip_3_shots/")
+@click.option("--shots", "-s", type=list, default=[16, 51, 57, 59, 61, 63, 65, 67, 69, 71, 72, 74, 78, 80, 84, 91, 95, 102, 165, 187, 260, 267])
+def add_shot_number(**kwargs):
+    from shot import add_shot_number
+    add_shot_number(**kwargs)
+
+@main.command()
+# Clip 1
+# @click.option("--input_dir", "-i", type=Path, default="output/clip_1_shots/")
 # @click.option("--output_dir", "-o", type=Path, default="output/clip_1/")
 # @click.option("--logo_path", "-d", type=Path, default="data/clip_1_logo.png")
 # @click.option("--min_threshold", "-t", type=float, default=0.87)
-# @click.option("--model_path", "-m", type=Path, default="svm_model.sav")
+# @click.option("--model_path", "-m", type=Path, default="models/svm_model.sav")
 # @click.option("--classification", "-c", default="SVM", help="SVM or NN_SIFT or CNN")
 # @click.option("--vid_name", "-v", default="clip_1")
 # @click.option("--fps", "-fps", default=6, type=int, help="Frame per second")
 # Clip 2
-# @click.option("--input_dir", "-i", type=Path, default="original_data/clip_2/")
+# @click.option("--input_dir", "-i", type=Path, default="output/clip_2_shots/")
 # @click.option("--output_dir", "-o", type=Path, default="output/clip_2/")
 # @click.option("--logo_path", "-d", type=Path, default="data/clip_2_logo.png")
 # @click.option("--min_threshold", "-t", type=float, default=0.82)
-# @click.option("--model_path", "-m", type=Path, default="svm_model.sav")
+# @click.option("--model_path", "-m", type=Path, default="models/svm_model.sav")
 # @click.option("--classification", "-c", default="SVM", help="SVM or NN_SIFT or CNN")
 # @click.option("--vid_name", "-v", default="clip_2")
 # @click.option("--fps", "-fps", default=6, type=int, help="Frame per second")
 # Clip 3
-@click.option("--input_dir", "-i", type=Path, default="original_data/clip_3/")
+@click.option("--input_dir", "-i", type=Path, default="output/clip_3_shots_logos/")
 @click.option("--output_dir", "-o", type=Path, default="output/clip_3/")
 @click.option("--logo_path", "-d", type=Path, default="data/clip_3_logo.png")
 @click.option("--min_threshold", "-t", type=float, default=0.82)
-@click.option("--model_path", "-m", type=Path, default="svm_model.sav")
+@click.option("--model_path", "-m", type=Path, default="models/svm_model.sav")
 @click.option("--classification", "-c", default="SVM", help="SVM or NN_SIFT or CNN")
 @click.option("--vid_name", "-v", default="clip_3")
 @click.option("--fps", "-fps", default=6, type=int, help="Frame per second")
@@ -105,9 +123,9 @@ def run_all(input_dir, output_dir, logo_path, min_threshold, model_path, \
     from utils import make_video
     logo_detection(input_dir=input_dir, output_dir=output_dir,\
         logo_path=logo_path, min_threshold=min_threshold)
-    face_detection_cascade(input_dir=output_dir, output_dir=output_dir,\
-        model_path=model_path, classification=classification)
-    make_video(imgs_dir=output_dir, vid_name=vid_name, fps=fps)
+    # face_detection_cascade(input_dir=output_dir, output_dir=output_dir,\
+    #     model_path=model_path, classification=classification)
+    # make_video(imgs_dir=output_dir, vid_name=vid_name, fps=fps)
 
 if __name__ == "__main__":
     main()
